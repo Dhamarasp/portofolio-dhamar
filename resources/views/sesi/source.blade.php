192476ff3cd7ead8ef48b54bd7f12dbe4f62b655
Dhamarasp <dhamarasp.10rpl1@gmail.com>
2022-11-15 21:24:16 +0700

    Memperbaiki title dihalaman login dan register

--- resources/views/sesi/source.blade.php
+++ resources/views/sesi/source.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Dhamar - @yield('title')</title>
+    <title>SB Admin 2 - @yield('title')</title>
 
     <!-- Custom fonts for this template-->
     <link rel="stylesheet" href={{ asset('template/vendor/fontawesome-free/css/all.min.css') }}  type="text/css">
